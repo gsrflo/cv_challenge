dd22fbc8feda43d8ebfa38197c84e2fd18c6fb13
fgeiser <fgeiser@users.noreply.github.com>
2020-06-24 11:52:14 +0200

    add mode overlay & substitute

--- render.m
+++ render.m
@@ -1,4 +1,4 @@
-%function [result] = render(frame,mask,bg,mode)
+function [result] = render(frame,mask,bg,mode)
 
 
 %% comment on function to get variables in matlab workspace
@@ -20,12 +20,16 @@ foreground = zeros(600,800);
 background = zeros(600,800);
 
 % ---- for debugging, add dummies -------
-mode = 2;
+input_image = imread("pic1.jpg");
+mode = 4;
+% dummy mask
 mask = ones(600,800);
 mask = triu(mask);
+% dummy bg
+bg = imread("pic2.jpg");
 %----------------------------------------
 
-input_image = imread("pic1.jpg");
+
 mask_inv = ones(size(mask)) - mask;
 
 % picture preperation
@@ -34,6 +38,10 @@ image_r = dbl_input_image(:,:,1);
 image_g = dbl_input_image(:,:,2);
 image_b = dbl_input_image(:,:,3);
 
+dbl_bg_image = double(bg);
+bg_r = dbl_bg_image(:,:,1);
+bg_g = dbl_bg_image(:,:,2);
+bg_b = dbl_bg_image(:,:,3);
 
 % distinguish modes
 switch mode
@@ -69,9 +77,52 @@ switch mode
     case 3
         % Case 3: overlay
         disp("case3 selected")
+        
+        % keep foreground
+        foreground_r = image_r .* mask;
+        foreground_g = image_g .* mask * 1.5;
+        foreground_b = image_b .* mask;
+
+        % keep background
+        background_r = image_r .* mask_inv;
+        background_g = image_g .* mask_inv;
+        background_b = image_b .* mask_inv * 1.5;
+        
+        % merge foreground and background
+        overlay_r = foreground_r + background_r;
+        overlay_g = foreground_g + background_g;
+        overlay_b = foreground_b + background_b;
+        
+        % put layers together
+        output_image(:,:,1) = overlay_r;
+        output_image(:,:,2) = overlay_g;
+        output_image(:,:,3) = overlay_b;
+        output_image = uint8(output_image);
+        
     case 4
         % Case 4: substitute
         disp("case4 selected")
+        
+        % keep foreground
+        foreground_r = image_r .* mask;
+        foreground_g = image_g .* mask;
+        foreground_b = image_b .* mask;
+        
+        % create background
+        background_r = bg_r .* mask_inv;
+        background_g = bg_g .* mask_inv;
+        background_b = bg_b .* mask_inv;
+        
+        % merge foreground and background
+        substitute_r = foreground_r + background_r;
+        substitute_g = foreground_g + background_g;
+        substitute_b = foreground_b + background_b;
+        
+        % put layers together
+        output_image(:,:,1) = substitute_r;
+        output_image(:,:,2) = substitute_g;
+        output_image(:,:,3) = substitute_b;
+        output_image = uint8(output_image);
     otherwise
         disp("Error: no mode selected")
 end
@@ -82,4 +133,4 @@ imshow(output_image)
 %hold on
 
 
-%end
+end
