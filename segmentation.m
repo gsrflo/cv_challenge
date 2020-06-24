02a23c5981b2f56c5d6d1d18c58b0ffc645f3931
johteutsch <joh.teutsch@gmail.com>
2020-06-24 15:57:18 +0200

    Update segmentation.m

--- segmentation.m
+++ segmentation.m
@@ -1,7 +1,81 @@
 function [mask] = segmentation(left,right)
-  % Add function description here
-  %
-  %
+  % This function determines the foreground-mask of the first image of the
+  % left camera (i.e., left(:,:,1:3) ).
 
+  %left: tensor of current image + N follow-up images from left camera
+  %right: tensor of the current image + N follow-up images from right camera
+  %mask: segmentation mask - entries with 1: foreground
+  
+  %% Background estimation
+  %Number of follow-up Images
+  N = size(left,3)/3 - 1;
+  
+  %Reduced number of follow-up images for background estimation (to
+  %increase speed by using less images)
+  Nr = 15;
+  
+  if(Nr>N)
+      Nr=N;
+  end
+  
+  %Initialize background image
+  bg = uint8(zeros(size(left(:,:,1:3))));
+  
+  %Estimate background via median of Nr follow-up images
+  bg(:,:,1) = uint8(median(left(:,:,1:3:(Nr+1)*3-2),3));
+  bg(:,:,2) = uint8(median(left(:,:,2:3:(Nr+1)*3-1),3));
+  bg(:,:,3) = uint8(median(left(:,:,3:3:(Nr+1)*3  ),3));
 
+  %% Compute foreground mask:
+  %Idea: the foreground is detected by substracting the background from the
+  %images and compare the error between the current image and the median 
+  %of all images.
+  
+  %tensor of repeated background image - same size as left
+  bgrep = repmat(bg,[1,1,size(left,3)/3]);
+  
+  %NOTE: substracting variables of type uint8 gives 0 instead of negative
+  %values! This is used here to give 'positive' and 'negative' deviations
+  %different weights.
+  
+  %positive deviation: image brighter than median
+  dev_pos = rgb2gray( left(:,:,1:3)-bg - median(left-bgrep,3) );
+  
+  %negative deviation: median brighter than image
+  dev_neg = rgb2gray( bg-left(:,:,1:3) - median(bgrep-left,3) );
+  
+  %bound for positive deviation:
+  bound_pos = 2;
+  
+  %bound for negative deviation:
+  bound_neg = 20;
+  
+  %foreground mask:
+  mask = (dev_pos > bound_pos) | (dev_neg > bound_neg);
+  
+  %getting rid of noise:
+  mask = imerode(mask,strel('disk',5));
+  
+  %smothing / filling out holes:
+  %mask = imdilate(mask,strel('disk',10));
+  mask = imclose(mask,strel('disk',200));
+
+  %convertion to uint8, so that the mask can later be used like: img.*mask
+  mask=uint8(mask);
+  
+  %% Alternative
+  
+  %Computer Vison Toolbox: Foreground Detector
+  %{
+  
+  %creating detector object:
+  detector = vision.ForegroundDetector;
+  
+  %learning-phase of detector using the background estimate
+  detector(bg);
+  
+  %foreground detection;
+  mask = detector(left(:,:,1:3));
+  
+  %}
 end
