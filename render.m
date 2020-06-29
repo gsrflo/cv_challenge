094764b62412149cea01479ee7e4a575bbfdafca
fgeiser <fgeiser@users.noreply.github.com>
2020-06-29 16:52:34 +0200

    add gif function

--- render.m
+++ render.m
@@ -12,17 +12,22 @@ function [result] = render(frame, mask, bg, mode)
 % 4 - substitute: substitute background with "bg"
 %
 
-debug = false;
+debug = true;
 
 % ---- for debugging, add dummies -------
 if debug
+    % dummy picture
     frame = imread("pic1.jpg");
-    mode = "substitute";
+    mode = "gif";
     % dummy mask
-    mask = uint8(ones(600, 800));
-    mask = triu(mask);
-    % dummy bg
+    mask1 = zeros(600, 450);
+    mask2 = ones(600, 200);
+    mask3 = zeros(600, 150);
+    mask = uint8([mask1 mask2 mask3]);
+    % dummy background
     bg = imread("pic4.jpg");
+    % dummy gif
+    gif = "nyan_cat.gif";
 end
 
 %----------------------------------------
@@ -37,12 +42,14 @@ switch mode
         % keep foreground, set background black
         result = frame .* mask;
         
+        imshow(result);
     case "background"
         % Case 2: background
         
         % keep background, set foreground black
         result = frame .* mask_inv;
         
+        imshow(result);
     case "overlay"
         % Case 3: overlay
         
@@ -72,6 +79,8 @@ switch mode
         result(:, :, 2) = overlay_g;
         result(:, :, 3) = overlay_b;
         
+        imshow(result);
+        
     case "substitute"
         % Case 4: substitute
         
@@ -86,11 +95,47 @@ switch mode
         
         % merge foreground and background
         result = foreground + background;
+        
+        imshow(result);
+        
+    case "gif"
+        % Case 5: put gif as background
+        
+        % load gif and define parameters
+        [gif_image, cmap] = imread(gif, 'Frames', 'all');
+        [~, ~, ~, num_images] = size(gif_image);
+        
+        % construct an empty movie by initializing dimensions
+        result = zeros(600, 800, 3, num_images, 'uint8');
+        
+        % loop thru every frame
+        for i = 1 : num_images
+            
+          % choose frame and convert it  
+          % Multiply with 255: colormaps read in from GIFs represent colors
+          % in the range 0 to 1, rather than integers 0 to 255
+          gif_frame = gif_image(:,:,:, i);
+          gif_frame_rgb = uint8(255 * ind2rgb(gif_frame, cmap));
+          
+          % resize image and define foreground and background
+          gif_frame_rgb = imresize(gif_frame_rgb, [600,800]);
+          foreground = frame .* mask;
+          background = gif_frame_rgb .* mask_inv;
+          
+          % merge foreground and background
+          single_pic = foreground + background;
+          
+          % set pictures to an 4D array
+          result(:,:,:,i) = single_pic;
+
+        end
+        
+        % show gif
+        implay(result);
+        
     otherwise
         disp("Error: no mode selected")
 end
-
-imshow(result);
 
 
 end
