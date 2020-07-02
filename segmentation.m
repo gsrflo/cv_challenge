ae73636b1a934aed8db5d1e2723fd0d3b5d774dd
johteutsch <67161941+johteutsch@users.noreply.github.com>
2020-07-02 15:23:10 +0200

    Updates on segmentation method

--- segmentation.m
+++ segmentation.m
@@ -1,4 +1,4 @@
-function [mask] = segmentation(left, right)
+ function [mask] = segmentation(left, right)
   % This function determines the foreground-mask of the first image of the
   % left camera (i.e., left(:,:,1:3) ).
 
@@ -9,6 +9,7 @@ function [mask] = segmentation(left, right)
   % Log:  - 20200622: Setting up initial structure
   %       - 20200624: Extended by alternative: vision.ForegroundDetector
   %       - 20200625: Minor updates + switch case to choose detector
+  %       - 20200702: Minor updates on segmentation
 
   % TODO: - finding parameters with best result
   %       - finding method with lowest computation time
@@ -17,21 +18,21 @@ function [mask] = segmentation(left, right)
   %Number of follow-up Images
   N = size(left, 3) / 3 - 1;
 
-  %Reduced number of follow-up images for background estimation (to
-  %increase speed by using less images)
-  Nr = 20;
-
-  if (Nr > N)
-    Nr = N;
-  end
-
   %Initialize background image
   bg = uint8(zeros(size(left(:, :, 1:3))));
 
   %Estimate background via median of Nr follow-up images
-  bg(:, :, 1) = uint8(median(left(:, :, 1:3:(Nr + 1) * 3 - 2), 3));
-  bg(:, :, 2) = uint8(median(left(:, :, 2:3:(Nr + 1) * 3 - 1), 3));
-  bg(:, :, 3) = uint8(median(left(:, :, 3:3:(Nr + 1) * 3), 3));
+  bg(:, :, 1) = uint8(median(left(:, :, 1:3:(N + 1) * 3 - 2), 3));
+  bg(:, :, 2) = uint8(median(left(:, :, 2:3:(N + 1) * 3 - 1), 3));
+  bg(:, :, 3) = uint8(median(left(:, :, 3:3:(N + 1) * 3    ), 3));
+  
+  %Grayimages:
+  bg=rgb2gray(bg);
+  
+  lft_gray=uint8(zeros(size(left,1), size(left,2),N));
+  for i=1:N
+        lft_gray(:,:,i)=rgb2gray(left(:,:,3*i-2:3*i));
+  end
 
   %% Compute foreground mask:
   
@@ -44,50 +45,82 @@ function [mask] = segmentation(left, right)
       case 0
         %% Compare to background
         %Idea: the foreground is detected by substracting the background from the
-        %images and compare the error between the current image and the median
-        %of all images.
-
-        %tensor of repeated background image - same size as left
-        bgrep = repmat(bg, [1, 1, size(left, 3) / 3]);
+        %images and compare the error between the floor(N/2)-th image and the background
 
         %NOTE: substracting variables of type uint8 gives 0 instead of negative
         %values! This is used here to give 'positive' and 'negative' deviations
         %different weights.
         
+        Nr = floor(N/2);
+        
+        bg_med = median(bg,'all');
+        
         %positive deviation: image brighter than median
-        dev_pos = rgb2gray(left(:, :, 1:3) - bg - median(left - bgrep, 3));
-
+        %dev_pos = rgb2gray(left(:, :, 3*Nr-2:3*Nr) - bg);
+        dev_pos = lft_gray(:, :, Nr) - bg + bg_med;
+        %dev_pos_med = lft_gray(:,:,Nr-1) - bg + bg_med;                %in case a comparison with the previos image is desired
+        
         %negative deviation: median brighter than image
-        dev_neg = rgb2gray(bg - left(:, :, 1:3) - median(bgrep - left, 3));
-
+        %dev_neg = rgb2gray(bg - left(:, :, 3*Nr-2:3*Nr));
+        dev_neg = bg - lft_gray(:, :, Nr) + bg_med;
+        %dev_neg_med = bg - lft_gray(:,:,Nr-1) + bg_med;                %in case a comparison with the previos image is desired
+        
         %bound for positive deviation:
-        bound_pos = 20;
+        bound_pos = 8;
 
         %bound for negative deviation:
-        bound_neg = 20;
+        bound_neg = 12;
         
-        %foreground mask:
-        mask = (dev_pos > bound_pos) | (dev_neg > bound_neg);
-
-        %getting rid of noise: imerode
-        %smothing / filling out holes: imdilate, imclose
-        %you can play with these (change order, params; leave one out;...):
+ 
+        %FOREGROUND MASK:
+        mask = (dev_pos - bg_med> bound_pos) | (dev_neg - bg_med> bound_neg);
+        %mask = (imabsdiff(dev_pos,dev_pos_med) > bound_pos) | (imabsdiff(dev_neg,dev_neg_med)> bound_neg);     %comparison with the previos image
         
-        mask = imclose(mask,strel('disk',3));
-        mask = imerode(mask,strel('disk',7));    
-        mask = imdilate(mask,strel('disk',7));
-        mask = imclose(mask,strel('disk',100));
-    
-        %convertion to uint8, so that the mask can later be used like: img.*mask
+        %delete single pixels and fill holes:        
+        mask = bwmorph(mask,'clean');
+        mask = bwmorph(mask,'fill');
+        
+        %group neighboring pixels to (small) clusters (in the hope that
+        %the pixels of the foreground combine to one cluster)
+        mask = imdilate(mask,strel('disk',1));
+        mask = imclose(mask,strel('disk',1));
+        
+        %delete all clusters whose pixel-area is below 30
+        mask = bwareafilt(logical(mask),[30,inf]);
+        
+        %(get edge of remaining clusters and) fill holes to get big cluster
+        mask = bwperim(mask,8);
+        mask = imfill(mask,'holes');
+        mask = imdilate(mask,strel('disk',4));
+        mask = imfill(mask,'holes');
+        
+        %delete all clusters whose pixel-area is below 6000
+        mask = bwareaopen(mask, 6000,8);
+        
+        %get closed holes at the image bottom (which can then be filled):
+        mask(end,find(mask(end,:),1,'first'):find(mask(end,:),1,'last'))=1;
+        
+        %fill remaining holes
+        mask=imclose(mask,strel('disk',50));       
+        mask=imfill(mask,'holes');    
+        
+        %convertion to uint8, so that the mask can later be used like: img.*mask      
         mask = uint8(mask);
-
+        
+        %imshow(mask*255);
+        
+        %OTHER FUNCTIONS OF INTEREST:
+        %imopen(mask,strel('object',size)) %erosion followed by dilation
+        %imclose(mask,strel('object',size)) %dilation followed by erosion
+        %bwmorph(mask,'method') %different morphological operations
+        %mask=bwconvhull(mask) %convex hull
       case 1
-        %% Alternative
+        %% Computer Vision Toolbox: ForegroundDetector 
 
         %Computer Vison Toolbox: Foreground Detector
         
         %number of training frames for learing phase;
-        num_trainframes = 20;
+        num_trainframes = 30;
         
         %creating detector object:
         detector = vision.ForegroundDetector('NumTrainingFrames',num_trainframes);
@@ -96,16 +129,38 @@ function [mask] = segmentation(left, right)
         for i=1:num_trainframes
         detector(bg);
         end
-
+        Nr = floor(N/2);
         %foreground detection;
-        mask = detector(left(:,:,1:3));
+        mask = detector(lft_gray(:,:,Nr));
 
         %you can play with these (change order, change parameters):
-        mask = imclose(mask,strel('disk',3));
-        mask = imerode(mask,strel('disk',7));    
-        mask = imdilate(mask,strel('disk',7));
-        mask = imclose(mask,strel('disk',100));
 
+        %delete single pixels and fill holes:        
+        mask=bwmorph(mask,'clean');
+        mask=bwmorph(mask,'fill');
+        
+        %group neighboring pixels to (small) clusters (in the hope that
+        %the pixels of the foreground combine to one cluster)
+        mask=imdilate(mask,strel('disk',1));
+        mask = imclose(mask,strel('disk',1));
+        
+        %delete all clusters whose pixel-area is below 10
+        mask=bwareafilt(logical(mask),[10,inf]);
+        
+        %fill holes to get big cluster
+        mask=imdilate(mask,strel('disk',3));
+        mask=imfill(mask,'holes');
+        
+        %delete all clusters whose pixel-area is below 1000
+        mask=bwareaopen(mask, 1000,8);
+        
+         %get closed holes at the image bottom (which can then be filled):
+        mask(end,find(mask(end,:),1,'first'):find(mask(end,:),1,'last'))=1;
+        
+        %fill remaining holes
+        mask=imclose(mask,strel('disk',40));       
+        mask=imfill(mask,'holes');
+              
         %convertion to uint8, so that the mask can later be used like: img.*mask
         mask = uint8(mask);
         
